811b475265d2dc18e98efe1f8d780f7166fda810
tangdahe <dahe.tang@pingcap.com>
2022-05-13 17:26:51 +0800

    done a sample envtest

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -17,8 +17,11 @@ limitations under the License.
 package controllers
 
 import (
+	"context"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"path/filepath"
 	"testing"
+	"time"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -53,11 +56,13 @@ var _ = BeforeSuite(func() {
 	logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
 
 	By("bootstrapping test environment")
+	// 指定 testEnv 配置
 	testEnv = &envtest.Environment{
 		CRDDirectoryPaths:     []string{filepath.Join("..", "config", "crd", "bases")},
 		ErrorIfCRDPathMissing: true,
 	}
 
+	// 启动 testEnv
 	cfg, err := testEnv.Start()
 	Expect(err).NotTo(HaveOccurred())
 	Expect(cfg).NotTo(BeNil())
@@ -73,8 +78,30 @@ var _ = BeforeSuite(func() {
 
 }, 60)
 
+var _ = Describe("node labels", func() {
+	pool := &webappv1.NodePool{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "test",
+		},
+		Spec: webappv1.NodePoolSpec{
+			Labels: map[string]string{
+				"node-pool.webapp.elasticweb-operator/xxx": "",
+			},
+			Handler: "",
+		},
+	}
+	It("create pool", func() {
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		err := k8sClient.Create(ctx, pool)
+		Expect(err).NotTo(HaveOccurred())
+	})
+})
+
 var _ = AfterSuite(func() {
 	By("tearing down the test environment")
+	// 停止测试
 	err := testEnv.Stop()
 	Expect(err).NotTo(HaveOccurred())
 })
